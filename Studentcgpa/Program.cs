071ea90456e9adb0598a200d0fb7e267c1a07276
osman goni <osman.ice.32@gmail.com>
2023-06-03 15:11:07 +0600

    first commit

---
+++ Studentcgpa/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Studentcgpa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("okk");
+           
+            StudenInput si1 = new StudenInput("jahid");
+            si1.Display();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Studentcgpa/StudenInput.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Studentcgpa
+{
+    internal class StudenInput
+    {
+        public string Name { get; set; }
+        public int Number { get; set; }
+
+        public StudenInput(String name) { 
+            
+            Name = name;
+
+        }
+        public void Display()
+        {
+            Console.WriteLine("we working on it.... enter marks");
+            Number=int.Parse(Console.ReadLine());
+            if(Number != 80)
+            {
+                Console.WriteLine($"{Name} have passed the exaam");
+            }
+        }
+    }
+}
